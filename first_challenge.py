6f44eff2a3aa7e556caedf419073ef3144dfc88f
PLosano <paulalosano13@gmail.com>
2024-03-14 15:59:37 -0300

    Add dotenv

--- first_challenge.py
+++ first_challenge.py
@@ -1,15 +1,17 @@
 import requests
+from dotenv import load_dotenv
+from dotenv import dotenv_values
 
 # Function to obtain the data
-def obtain_data(url, api_key, start_date, end_date):
+def obtain_data(config, start_date, end_date):
     try:
         # Define the request parameters, including the API key and dates
-        params = {'api_key': api_key, 
+        params = {'api_key': config["api_key"], 
                   'start_date': start_date,
                   'end_date': end_date}
 
         # Make the request to the API with the parameters
-        api_response = requests.get(url, params=params)
+        api_response = requests.get(config["url_api"], params=params)
 
         if api_response.status_code == 200:
             return api_response.json()
@@ -21,12 +23,11 @@ def obtain_data(url, api_key, start_date, end_date):
         print(f"Error: {e}")
         return None
 
-url_api = "https://api.nasa.gov/neo/rest/v1/feed"
-api_key = "Vf6Ex970rHCGrFjWtJffcRXHXWm8IHOaxwrythDm"
+config = dotenv_values(".env")
 start_date = '2023-02-20'
 end_date = '2023-02-27'
 
-data = obtain_data(url_api, api_key, start_date, end_date)
+data = obtain_data(config, start_date, end_date)
 
 if data:
     print("API Data:")
